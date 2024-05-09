fd4128932fa41b150e49573881fa6ad3f598fe58
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-05-09 18:24:19 -0300

    Create simulator.py

---
+++ coders/simulator.py
@@ -0,0 +1,13 @@
+print( 'Welcome to your adventure simulator.')
+print('I am going to ask you a bunchof questions and then create an epic story with you as the star!')
+print()
+name= input('What is your name? ')
+enemy= input ("What is your worst enemy's name? ")
+superpower = input "What is your superpower? ')
+place = input('Where do you live? ')
+food = input('What is your favorite food?')
+print()
+print('Hello ', '\033[33m', name,'\033[0m','! Your ability to ',
+'\033[35m', superpower, '\033[0m', ' will make sure you never have to look at ', 
+'\033[31m', enemy,'\033[0m', ' again. Go eat ', food, ' as you walk down the
+streets of ', place, 'for good and not evil.')
