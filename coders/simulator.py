f25e8a2752714f9d4756ae8c2c3929558ccd7c6a
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-05-09 19:34:01 -0300

    Update simulator.py

--- coders/simulator.py
+++ coders/simulator.py
@@ -9,5 +9,4 @@ food = input('What is your favorite food?')
 print()
 print('Hello ', '\033[33m', name,'\033[0m','! Your ability to ',
 '\033[35m', superpower, '\033[0m', ' will make sure you never have to look at ', 
-'\033[31m', enemy,'\033[0m', ' again. Go eat ', food, ' as you walk down the
-streets of ', place, 'for good and not evil.')
+'\033[31m', enemy,'\033[0m', ' again. Go eat ', food, ' as you walk down the streets of ', place, 'for good and not evil.')
