57a79c9b887a473a064906effad9467e224b1c29
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-05-16 21:51:50 -0300

    Update fibonacci.py

--- coders/fibonacci.py
+++ coders/fibonacci.py
@@ -1,18 +1,17 @@
-qtd_interacao= int(input("Informe o termo da Fibonacci você deseja saber: "))
-contador=3
-valor1=1
-valor2=1
-valor3=0
+qtd_interacao = int(input("Informe o termo da Fibonacci que você deseja saber: "))
+contador = 2
+valor1 = 0
+valor2 = 1
+valor3 = 0
 
-if qtd_interacao -= 0:
+if qtd_interacao == 0:
    print("O primeiro termo da sequência é: 0")
-
 elif qtd_interacao == 1:
    print("O segundo termo da sequência é: 1")
 else:
    while qtd_interacao > contador:
-      valor3= valor1+ valor2
-      valor1=valor2
-      valor2=valor3
-      contador+= 1
-      print("0 termo {} da sequência de Fibonnaci é: {:_}".format(qtd_interacao, valor3).replace("_","."))
+      valor3 = valor1 + valor2
+      valor1 = valor2
+      valor2 = valor3
+      contador += 1
+   print("O termo {} da sequência de Fibonacci é: {}".format(qtd_interacao, valor3)
