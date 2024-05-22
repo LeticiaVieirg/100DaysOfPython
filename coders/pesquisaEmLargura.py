32cd7a09dfc8843e0cc67a352fa4fb468e8c782f
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-05-21 21:14:43 -0300

    Update pesquisaEmLargura.py

--- coders/pesquisaEmLargura.py
+++ coders/pesquisaEmLargura.py
@@ -1,1 +1,33 @@
+from collections import deque
 
+grafo={ 
+'Clara': ['Diana', 'Aline', 'Joana'],
+'Diana': ['Luiza', 'Bruna'],
+'Aline': ['Italo', 'Pedro'],
+'Joana': ['Bruna'],
+'Luiza': [],
+'Bruna': [],
+'Italo': [],
+'Pedro': []
+}
+
+def pessoaAtleta(nome):
+  return nome[-1]=='m'
+
+def pesquisar(nome):
+  filaPesquisa=deque(nome)
+  filaPesquisa+=grafo[nome]
+  pessoasVerificadas=[]
+  
+  while filaPesquisa:
+    pessoa=filaPesquisa.popleft()
+    if pessoa not in pessoasVerificadas:
+       if pessoaAtleta(pessoa):
+         print(f'{pessoa} eh um atleta.')
+         return True
+       else:
+         filaPesquisa+=grafo[pessoa]
+         pessoasVerificadas.append(pessoa)   
+   return False
+
+pesquisa('Clara')
