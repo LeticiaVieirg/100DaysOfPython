01faf5c7a7055540a05c8bd47953c2cd3098d653
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-05-28 00:02:12 +0000

    Create 11algGulosos.py

---
+++ coders/11algGulosos.py
@@ -0,0 +1,29 @@
+#ALGORITMOS GULOSOS
+# Em cada etapa, escolhemos a solucao ideial para que no final, a solucao global ideal esta formada
+# De facil compreensao e rapida execucao
+
+#List of states to include and creat hash table 
+states_include=set(["ab", "cd", "ef", "gh", "ij", "kl", "mn","op"])
+stations={}
+stations["Ione"]=set(["ab", "cd", "ef")]
+stations["Itwo"]=set(["op", "ab", "ij"])
+stations[("Ithree")]=set(["mn", "cd", "kl"])
+stations[("Ifour")]=set(["cd", "ef"])
+stations[("Ifive")]=set(["kl", "gh"])
+
+#Store the final set of stations, best station and states for covered
+stations_end=set()
+
+#As long as states need coverage
+while states_include:
+  best_station = None
+  states_covered = set()
+  for station, states_station in stations.items():
+    covered=states_include & states_stations:
+      if len(covered)>len(states_covered):
+        best_station=station
+        states_covered=covered
+states_include-=states_covered
+stations_end.add(best_station)
+
+print(stations_end)
