65b69bde5518a2c86dda53feb73119f89d926385
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-06-01 12:55:45 -0400

    Create 14strings.py

---
+++ coders/14strings.py
@@ -0,0 +1,12 @@
+#iinteroplação de strings
+a='Book'
+
+#concatencao
+print('She has a '+a+ ' green!')
+
+#interpolaçao
+print('The height of %s is %d!' (s, len(s)))
+
+for ch in s: print ch
+  if s.startswith('C'): print (s.upper())
+    print (3*s)
