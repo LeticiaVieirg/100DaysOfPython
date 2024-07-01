0c7f218f2e15fa9f6d41e2bc271ef5fbf520667c
Letícia Vieira Gonçalves <90807534+LeticiaVieirg@users.noreply.github.com>
2024-07-01 16:43:30 -0400

    Update sinCosTan.py

--- coders/calculus/sinCosTan.py
+++ coders/calculus/sinCosTan.py
@@ -6,3 +6,5 @@ cosine=cos(radians(angle))
 tangent=tan(radians(angle))
 
 print('The sine value of the angle {} is: {:.2f}'.format(angle, sin))
+print('The cosine value of the angle {} is {:.2f}'.format(angle, cos))
+print('The tangent value of the angle {} is {:.2f}'.format(angle, tangent))
