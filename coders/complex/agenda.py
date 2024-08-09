06cd13af0036320506cf9a6324eb0cadca5440b2
Letícia Vieira Gonçalves <90807534+LeticiaVieirg@users.noreply.github.com>
2024-08-08 21:10:52 -0400

    Update agenda.py

--- coders/complex/agenda.py
+++ coders/complex/agenda.py
@@ -85,3 +85,114 @@ def export_contacts(file_agenda):
 
 def save_agenda():
     export_contacts('database.csv')
+
+
+def import_contacts (file_agenda):
+    try: 
+        with open(file_agenda, 'r') as file:
+            lines=file.readlines()
+            for line in lines:
+                details=line.strip().split(',')
+
+                name=details[0]
+                phone_number=details[1]
+                email=details[2]
+
+                include_edite_contact(name, phone_number, email)
+    except FileNotFoundError:
+        print('File dont founded.')
+    except Exception as error:
+        print('Unexpedted error. ')
+        print(error)
+        
+def load_agenda():
+    try:
+        with open('database.csv', 'r') as file:
+            lines=file.readlines()
+            for line in lines:
+                details=line.strip().split(',')
+                
+                name=details[0]
+                phone_number=details[1]
+                email=details[2]
+
+                AGENDA[name]={
+                    'Phone Number': phone_number,
+                    'Email'=email
+                }
+            print('Load database sucessfully. ')
+            print('f{len(AGENDA)} Loaded contacts')
+    except FileNotFoundError: 
+        print('File not founded.')
+    except Exception as error:
+        print('Unexpedted error.')
+        print(error)
+
+def menu():
+    print('='*20)
+    print('1 - Show contacts in agenda')
+    print('2 - Search contact')
+    print('3 - Include contact')
+    print('4 - Edit contact)
+    print('5 - Delete contact')
+    print('6 - Export contact for CSV. ')
+    print('7 - Import contact for CSV. ')
+    print('0 - Close Agenda)
+    print('-' * 20)
+
+
+    load_agenda()
+    while True:
+        menu()
+
+        option=input('Choice a option: ')
+        if option =='1':
+            show_contacts()
+        elif option =='2':
+            contact=input('Enter a name of the contact: ')
+            contact_search(contact)
+        elif option =='3':
+            contact=input('Enter a name of the contact: ')
+            
+            try:
+                AGENDA[contact]
+                print('Existing contact.')
+            except KeyError:
+                phone_number, email=read_details_contact()
+                include_edite_contact(contact, phone_number, email)
+        elif option=='4':
+            contact=input('Enter a name of the contact: ')
+
+            try: 
+                AGENDA[contact]
+                print('Editing existing contact. ', contact)
+                phone_number, email=read_details_contact()
+                include_edite_contact(contact, phone_number, email)
+            except KeyError:
+                print('Unexpeted contact. ')
+    
+    elif option == '5':
+        contact = input('Enter a name of the contact: ')
+        delete_contact(contact)
+    elif opcao == '6':
+        file_agenda = input('Enter a name of the exported file: ')
+        export_contacts(file_agenda)
+    elif opcao == '7':
+        file_agenda = input('Enter a name of the imported file:  ')
+        import_contacts(file_agenda)
+    elif opcao == '0':
+        print('=' * 20)
+        print('Close program.')
+        print('=' * 20)
+        break
+    else:
+        print('Invalid option. ')
+
+
+
+
+
+
+
+
+
